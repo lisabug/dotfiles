7e916ec429e3d4cf6098b362ad5830f4daa6b118
lisabug <luyuanqin09@gmail.com>
2018-02-23 10:36:08 +0800

    add mount script

---
+++ mount.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+sudo mount -t nfs truenas.bj.tusimple.ai:/mnt/tank/scratch /mnt/truenas/scratch
+sudo mount -t nfs truenas.bj.tusimple.ai:/mnt/tank/upload /mnt/truenas/upload
+sudo mount -t nfs truenas.bj.tusimple.ai:/mnt/tank/team/algorithm /mnt/truenas/algorithm
