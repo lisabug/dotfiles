ca7cfb86715f33580d44f9e2430a3e1b93ac1ccb
lisabug <luyuanqin09@gmail.com>
2018-03-05 15:52:50 +0800

    add dataset

--- mount.sh
+++ mount.sh
@@ -3,3 +3,4 @@
 sudo mount -t nfs truenas.bj.tusimple.ai:/mnt/tank/scratch /mnt/truenas/scratch
 sudo mount -t nfs truenas.bj.tusimple.ai:/mnt/tank/upload /mnt/truenas/upload
 sudo mount -t nfs truenas.bj.tusimple.ai:/mnt/tank/team/algorithm /mnt/truenas/algorithm
+sudo mount -t nfs truenas.bj.tusimple.ai:/mnt/tank/datasets /mnt/truenas/datasets
